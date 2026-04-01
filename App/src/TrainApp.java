6eeb9a0b58506e4417c9d15f886b5966f5d85b73
Ardhendu Tripathy <5253447ardhendusekhartripathy9@gmail.com>
2026-04-01 09:42:27 +0530

    UC2 Add Passenger Bogies using ArrayList

---
+++ App/src/TrainApp.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> passengerBogies = new ArrayList<>();
+
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("Passenger Bogies: " + passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+
+        System.out.println("After Removal: " + passengerBogies);
+
+        boolean exists = passengerBogies.contains("Sleeper");
+        System.out.println("Sleeper exists: " + exists);
+
+        System.out.println("Final Bogie List: " + passengerBogies);
+    }
+}
